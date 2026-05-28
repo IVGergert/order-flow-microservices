60098e932dcabd6b2501ea8702752fa77595313d
Ilya Gergert <ilyagergert@gmail.com>
2026-05-28 11:29:37 +0300

    init: created docker-compose with 3 DBs, configured environment variables, and added order-service skeleton

---
+++ order-service/src/test/java/com/gergert/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gergert.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
